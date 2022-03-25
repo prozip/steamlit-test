7c868c11d5e543d8058dd82668825e3a5a2524ab
prozip <quachkimson2000@gmail.com>
2022-03-25 08:24:27 +0700

    print cv2

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 import tensorflow as tf
 from PIL import Image
+import cv2
 import os
 
 UPLOADED_PATH = 'uploaded_file/'
@@ -27,6 +28,7 @@ def main():
     st.title("this is title")
     st.write("hello world")
     st.write("Tensorflow: ", tf.__version__)
+    st.write("cv2: ", cv2.__version__)
     uploaded_file = st.file_uploader("Choose a file")
     if uploaded_file is not None:
         bytes_data = uploaded_file.getvalue()
@@ -34,7 +36,7 @@ def main():
             binary_file.write(bytes_data)
     if st.button('Run'):
         os.system("rm -r yolov5/runs")
-        os.system("python yolov5/detect.py --weights yolov5/yolov5l.pt --img 640 --conf 0.25 --source uploaded_file")
+        # os.system("python yolov5/detect.py --weights yolov5/yolov5l.pt --img 640 --conf 0.25 --source uploaded_file")
         image = Image.open('yolov5/runs/detect/exp/' + uploaded_file.name)
         st.image(image, caption='Finised')
 
